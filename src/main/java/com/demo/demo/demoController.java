11a0d55001d14112a6ab351df50fdfccdd799c62
petchiamdocs <petchiappanitt@gmail.com>
2024-07-04 15:25:17 +0530

    first commit

---
+++ src/main/java/com/demo/demo/demoController.java
@@ -0,0 +1,12 @@
+package com.demo.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class demoController {
+	@GetMapping("/msg")
+	public String show() {
+		return "welcome to jenkins";
+	}
+
+}
